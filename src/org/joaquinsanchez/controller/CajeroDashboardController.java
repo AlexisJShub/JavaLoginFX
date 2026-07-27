5a4b02ebe12ac8ff054debc229ef8d108cae659f
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-26 23:37:31 -0600

    013 feat(controller) Se agrego y termino el dashboard de interfaz para cajero

--- src/org/joaquinsanchez/controller/CajeroDashboardController.java
+++ src/org/joaquinsanchez/controller/CajeroDashboardController.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.controller;
 
-/**
- *
- * @author joaqu
- */
-public class CajeroDashboardController {
-    
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.joaquinsanchez.model.Usuario;
+
+public class CajeroDashboardController implements Initializable {
+
+    @FXML
+    private Label lblBienvenida;
+
+    @FXML
+    private Button btnSalir;
+
+    private Usuario usuarioActual;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    public void iniciarUsuario(Usuario usuario) {
+        this.usuarioActual = usuario;
+
+        lblBienvenida.setText(
+                "Bienvenido cajero " + usuario.getUsername()
+        );
+    }
+
+    @FXML
+    private void eventoSalir(ActionEvent evento) {
+
+        try {
+            FXMLLoader cargadorFXML = new FXMLLoader(
+                    getClass().getResource(
+                            "/org/js/view/InicioSesionView.fxml"
+                    )
+            );
+
+            Parent raiz = cargadorFXML.load();
+
+            Stage escenarioInicio = new Stage();
+            escenarioInicio.setScene(new Scene(raiz));
+            escenarioInicio.setTitle("Inicio de sesión");
+            escenarioInicio.show();
+
+            Stage escenarioActual =
+                    (Stage) btnSalir.getScene().getWindow();
+
+            escenarioActual.close();
+
+        } catch (IOException e) {
+            System.err.println(
+                    "Error al regresar al inicio de sesión: "
+                    + e.getMessage()
+            );
+
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
 }
