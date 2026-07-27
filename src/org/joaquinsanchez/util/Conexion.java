4b9b584c55f4ea17453bfddf98fd40365d08e605
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-26 23:19:50 -0600

    006 feat(conexion) Se hizo la conexion a la base de datos de login_db_in4cm

--- src/org/joaquinsanchez/util/Conexion.java
+++ src/org/joaquinsanchez/util/Conexion.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.util;
 
-/**
- *
- * @author joaqu
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
-    
+
+    private static Conexion instancia;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/javalogindb_in4cm?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "bZ7#qW9!fL";
+
+    private Conexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error Driver: " + e.getMessage());
+        }
+    }
+
+    public static synchronized Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+
+        return instancia;
+    }
+
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
