ffcd8cce19183baa0a1a08f8b7094f5b491d50e0
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-26 23:18:01 -0600

    005 feat(model) Se agrego y termino el modelo para usuario

--- src/org/joaquinsanchez/model/Usuario.java
+++ src/org/joaquinsanchez/model/Usuario.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.model;
 
-/**
- *
- * @author joaqu
- */
+import java.sql.Timestamp;
+
 public class Usuario {
-    
+
+    private int id;
+    private String username;
+    private String rol;
+    private boolean activo;
+    private Timestamp fechaCreacion;
+
+    public Usuario() {
+    }
+
+    public Usuario(
+            int id,
+            String username,
+            String rol,
+            boolean activo,
+            Timestamp fechaCreacion
+    ) {
+        this.id = id;
+        this.username = username;
+        this.rol = rol;
+        this.activo = activo;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Timestamp getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Timestamp fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
 }
